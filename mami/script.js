b32d0855128ef24d67b9c26ae6d4b2457624126d
carveler <3heyaberlin@gmail.com>
2020-10-26 16:08:36 +0100

    mami

---
+++ mami/script.js
@@ -0,0 +1,15 @@
+const massMark = 70;
+const heightMark = 1.69;
+const massJohn = 90;
+const heightJohn = 1.90;
+
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / (heightJohn **2);
+console.log(bmiMark, bmiJohn);
+
+if (bmiMark > bmiJohn) {
+  console.log( 'Is Marks BMI higher than Johns?', 'it is true, it is' )
+} else {
+  console.log('Is Marks BMI higher than Johns?', 'No it is not true')
+}
